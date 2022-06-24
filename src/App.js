3f6dbafbe7287b3164214e749961977d66ba8ef4
saifulemon <463saiful0@gmail.com>
2022-06-25 02:22:34 +0600

    installed react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Button varient='primary'>Hello</Button>
     </div>
   );
 }
