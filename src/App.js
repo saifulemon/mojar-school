960a07f38a11e0e68ad2c9439bbaf338521d8306
saifulemon <463saiful0@gmail.com>
2022-06-25 02:42:55 +0600

    added some components and pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
 
 
 function App() {
   return (
     <div className="App">
-      <Button varient='primary'>Hello</Button>
     </div>
   );
 }
